f719d578d636d23c477625b6fb3cb6a4a47f8262
Ricardo Presentacion <pardovelcari111@gmail.com>
2025-05-29 13:59:56 -0600

    login terminado, iniciando inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
- 
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { BrowserRouter, Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import InicioSesion from "./views/inicioSesion";
 import Reset_contra from './views/reset_contra';
 import Inicio from './views/Inicio';
@@ -10,15 +10,16 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>    
-      <Routes>
-        <Route path="/InicioSesion" element={<InicioSesion/>}></Route>
-        <Route path="/reset_contra" element={<Reset_contra/>}></Route>
-        <Route path="/inicio" element={<Inicio/>}></Route>
-      </Routes>
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/InicioSesion" element={<InicioSesion />}></Route>
+          <Route path="/reset_contra" element={<Reset_contra />}></Route>
+          <Route path="/inicio" element={<Inicio />}></Route>
  
+        </Routes>
+      </BrowserRouter>
+
+
     </>
   )
 }
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,41 +1,57 @@
 import { useState } from "react";
 
-export default function Inicio(){
+export default function Inicio() {
 
     const [user, setUser] = useState(false);
-    
-    return(        
-    <div className="bg-[#273240] relative h-screen w-screen flex flex-row items-center justify-around p-0 m-0 overflow-hidden">
-        <div className="flex flex-col min-w-1/6  text-white text-4xl">
-            <div>
-                <div>
-                    {   user?
-                        <img src="" alt="" />:
-                        <div className="rounded-full border-white border-2 w-[80px] h-[80px] flex items-center justify-center">
-                                <i class="fa-solid fa-user"></i>
-                        </div>
-                        
-                    }
-                </div>
-                <div className="text-xl">
-                    <i class="fa-solid fa-lightbulb"></i>
-                    <i class="fa-solid fa-right-from-bracket"></i>
-                </div>
-            </div>
-            <nav>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </nav>
-        </div>
-        <div className="bg-white min-w-screen md:min-w-4/6 min-h-[90vh] rounded-4xl">
+    const userName = 'user name', userEmail = 'mail@mail.com';
 
+    return (
+        <div className="bg-[#273240] relative h-screen w-screen flex flex-row items-center justify-around p-0 m-0 overflow-hidden">
+            <div className="flex flex-col min-w-1/6  text-white text-4xl">
+                <div className="">
+                    <div>
+                        <div>
+                            {user ?
+                                <img src="" alt="" /> :
+                                <div className="rounded-full border-white border-2 w-[80px] h-[80px] flex items-center justify-center">
+                                    <i className="fa-solid fa-user"></i>
+                                </div>
+
+                            }
+                        </div>
+                        <div >
+                            <h1  >
+                                {userName}
+                            </h1>
+                            <p className="text-lg opacity-50">
+                                {userEmail}
+                            </p>
+                        </div>
+
+                    </div>
+                    <div className="text-lg flex gap-4 py-4">
+                        <i className="fa-solid fa-sun"></i>
+                        <i className="fa-solid fa-right-from-bracket"></i>
+                    </div>
+                </div>
+                <nav>
+                    <ul className="text-xl">
+                        <li> <i className="fa-solid fa-house"></i> Inicio  </li>
+                        <li> <i className="fa-solid fa-users"></i> Administrar Usuarios</li>
+                        <li>  <i className="fa-solid fa-paperclip"></i> Reportes</li>
+                        <li>
+                            <div className="rounded-full border-white border-2 min-w-[40px] min-h-[40px]   items-center justify-center inline">
+                                <i className="fa-solid fa-user"></i>
+                            </div>Cuenta
+                        </li>
+                        <li><i className="fa-solid fa-wrench"></i> Configuracion</li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="bg-white min-w-screen md:min-w-4/6 min-h-[90vh] rounded-4xl">
+
+            </div>
         </div>
-    </div>
-);
+    );
 
 }
